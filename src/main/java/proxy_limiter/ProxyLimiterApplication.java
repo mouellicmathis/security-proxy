2b204f5e0723f1f334f282d2f9ce1ed0cf322be5
MxthisMM <mouellicmathis@gmail.com>
2026-07-03 23:06:04 +0200

    Initialisation du projet

---
+++ src/main/java/proxy_limiter/ProxyLimiterApplication.java
@@ -0,0 +1,13 @@
+package proxy_limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proxy_limiter/ProxyLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package proxy_limiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
